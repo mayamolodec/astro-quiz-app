5397b62ea880986e2ca83525c4b541fe056ee0d4
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-02-04 16:54:06 +0100

    Show page with quiz list

--- reactFirstProject/src/App.jsx
+++ reactFirstProject/src/App.jsx
@@ -1,13 +1,72 @@
 import { useState } from 'react'
-import './App.css'
+import ProgressBar from './ProgressBar'
+import { quizes } from './data.js';
+
+
+
+
+let states = [
+
+  // Imagine we have a state, where we keep the status of each quiz.
+  // For now it's just an array of objects
+
+  {
+    quizId:0,
+    currentQuestionNumber:0,
+    score:0
+  },
+
+  {
+    quizId:1,
+    currentQuestionNumber:3,
+    score:2
+  },
+  {
+    quizId:2,
+    currentQuestionNumber:4,
+    score:3
+  }
+]
+
+
+function ListQuizes({quizList}){
+  let score = "";
+
+  function openQuiz(e){
+    //goToQuiz
+  }
+
+  function restart(state){
+    score = state.score;
+    return (<>Restart</>)
+  }
+  
+  const listQuizes = quizList.map(quiz => 
+    <button onClick = {openQuiz} key = {quiz.id} value = {quiz.id}>
+      <p>Quiz #{quiz.id}</p>
+
+      <p>{
+      (states[quiz.id].currentQuestionNumber == quiz.quiz.length) ? restart(states[quiz.id]): 
+      (states[quiz.id].currentQuestionNumber == 0) ? "Start": "Continue"
+      }
+      </p>
+
+      <ProgressBar totalScore = {score} curQuestionId={states[quiz.id].currentQuestionNumber} questionsLength={quiz.quiz.length}/>
+   </button>
+    
+    );
+
+return(
+<div className='listQuizesContainer'>{listQuizes}</div>
+)
+}
 
 function App() {
 
   return (
     <>
-
-      <h1>Empty project</h1>
-
+    <h1>AstroQuiz</h1>
+    <ListQuizes quizList={quizes}/>
     </>
   )
 }
---
+++ reactFirstProject/src/data.js
@@ -0,0 +1,93 @@
+
+
+const quiz_1 = [{
+    img_link: "assets/1illustr_0.png",
+    text: "Quelle etoile est la plus brillante sur le ciel de nuit?",
+    answers:
+    ["Altair", "Polaire", "Sirius", "Venus"],
+    correct:3,
+    index: 0
+},
+
+{
+    img_link: "assets/1illustr_1.png",
+    text: "Lequel de ces objets est le plus loin de nous?",
+    answers:
+    ["Lune", "Mars", "Station spatiale internationale","Etoile filante"],
+    correct:2,
+    index: 1
+},
+
+{
+    img_link: "assets/1illustr_2.png",
+    text: "Quelle image montre une éclipse lunaire ?",
+    answers:
+    ["1", "2", "3","4"],
+    correct:2,
+    index: 2
+}
+,
+
+{
+    img_link: "assets/1illustr_3.png",
+    text: "Qu’est-ce qui n’est pas le nom de constellation ?",
+    answers:
+    ["Hercules", "Horloge", "Coquelicot","Paon"],
+    correct:3,
+    index: 3
+}
+];
+
+const quiz_2 =[
+    {
+        img_link: "./assets/2illustr_0.png",
+        text: "Laquelle parmi ces photos en longue exposition a été prise sur l'équateur?",
+        answers:
+        ["1", "2", "3", "4"],
+        correct:1,
+        index: 0
+    },
+    
+    {
+        img_link: "./assets/2illustr_1.png",
+        text: "Mais qu’est-ce que c’est q’ça?",
+        answers:
+        ["Etoile filante", "C’est un problème de caméra", "OVNI", "Comète"],
+        correct:4,
+        index: 1
+    },
+
+    {
+        img_link: "./assets/2illustr_2.png",
+        text: "On observe une éclipse lunaire: sur quelle phase de Lune on est?",
+        answers:
+        ["Nouvelle Lune", "Premiere/Derniere quartie", "Pleine Lune", "Toutes ces options sont possibles"],
+        correct:3,
+        index: 2
+    },
+    {
+        img_link: "./assets/2illustr_3.png",
+        text: "On voit un image du ciel crépusculaire clair (juste après le coucher du Soleil). \
+        C’est vraiment clair, on ne voit que la Lune et un objet lumineux tout près. Qu’est-ce que c’est ?",
+        answers:
+        ["Etoile Polaire", "SSI (Station Spatiale Internationale)","Venus", "L’avion"],
+        correct:3,
+        index: 3
+    }
+]
+
+export const quizes = [
+    {
+        id: 0,
+        quiz:quiz_1
+    },
+    {
+        id: 1,
+        quiz:quiz_2
+    },
+    {
+        id: 2,
+        quiz:quiz_1
+    }
+
+]
