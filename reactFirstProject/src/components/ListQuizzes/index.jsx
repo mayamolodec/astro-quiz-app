b3327cd69292d9c14d65471d0f943fc649910950
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-02-19 21:10:09 +0100

    Apply css.modules and scss

--- reactFirstProject/src/components/ListQuizzes/index.jsx
+++ reactFirstProject/src/components/ListQuizzes/index.jsx
@@ -1,16 +1,20 @@
 import { useSelector } from "react-redux";
 import QuizButton from "../QuizButton";
+import styles from "./ListQuizzes.module.scss"
+
+
 
 export default function ListQuizzes(){
+    console.log(styles);
     const states = useSelector(state => state.quizzes.quizzes);
     const listQuizes = states.map(state => {
       let status = (state.score === null) ? "Start" : "Restart";
       let score = (state.score === null) ? "" : state.score+"/"+state.questions;
   
       return(
-        <QuizButton name = {state.name} status = {status}  score={score}/>
+        <QuizButton name = {state.name} status = {status}  score={score} key={state.id}/>
       );
     });
   
-    return <div className='listQuizesContainer'>{listQuizes}</div>
+    return <div className={styles.container}>{listQuizes}</div>
     }
