5edb63a126dc3b5ec82d78a88479a0fbaddb50c3
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-04-02 16:27:16 +0200

    Remove quizSlice

--- reactFirstProject/src/store/index.js
+++ reactFirstProject/src/store/index.js
@@ -1,11 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
-// import quizReduser from './quizSlice'
 import { quizApi } from './quizApi';
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store =  configureStore({
     reducer:{
-        // quizzes: quizReduser,
         [quizApi.reducerPath]: quizApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
