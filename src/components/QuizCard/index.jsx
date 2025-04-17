eb276414025361830534d9061b0b5b3c3099f019
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-04-17 17:34:09 +0200

    Add QuizCard, fix router-router-dom import

--- src/components/QuizCard/index.jsx
+++ src/components/QuizCard/index.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import { useParams } from "react-router-dom"
 
+import styles from "./QuizCard.module.scss";
+
 export default function QuizCard() {
     const { id } = useParams();
+    const placeHolderImg = "https://apod.nasa.gov/apod/image/2412/MarsClouds_Perseverance_2048.jpg";
 
-    return <h1>Quiz Card {id}</h1>
+    return (
+        <>
+            <div className={styles["container"]}>
+                <form className={styles["container__card"]}>
+                    <img className={styles["container__card-img"]} src={placeHolderImg} />
+                    <div className={styles["container__card-text"]}>
+                        <h2 className={styles["container__card-text-question"]}>
+                            % QuestionText %
+                        </h2>
+                        <div className={styles["container__card-text-options"]}>
+                            <label className={styles["container__card-text-options-element"]}>
+                                <input type="radio" value="option1" name="answer" />
+                                % opt1 %
+                            </label>
+                            <label className={styles["container__card-text-options-element"]}>
+                                <input type="radio" value="option2" name="answer" />
+                                % opt2 %
+                            </label>
+                            <label className={styles["container__card-text-options-element"]}>
+                                <input type="radio" value="option3" name="answer" />
+                                % opt3 %
+                            </label>
+                            <label className={styles["container__card-text-options-element"]}>
+                                <input type="radio" value="option4" name="answer" />
+                                % opt4 %
+                            </label>
+                        </div>
+                    </div>
+                    <button className={styles["container__card-button"]}>
+                        % Submit %
+                    </button>
+                </form>
+            </div>
+            <p style={{ color: "grey" }}>Quiz number {id}</p>
+        </>
+
+    )
+
 }
