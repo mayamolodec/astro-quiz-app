b61b840bcdad435e0c583dbec96f4dcf6652a1da
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-04-29 17:21:22 +0200

    Switch to react form hook

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,58 +1,49 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useForm } from "react-hook-form";
 
 import regiFormImg from "../../assets/Frame5_2.svg";
 
 import styles from "./RegisterForm.module.scss"
 
 export default function RegisterForm() {
-    const [formData, setFormData] = useState({ login: "", email: "", password: "", passwordCheck: "" });
+    const { register, handleSubmit, formState: { errors }, watch } = useForm({ defaultValues: { login: "", email: "", password: "", passwordCheck: "" } });
     const [shake, setShake] = useState(false);
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+    const password = useRef({});
+
+    password.current = watch("password");
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+    const onInvalid = () => {
+        setShake(true);
+        setTimeout(() => setShake(false), 500);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (!formData.login || !formData.email || !formData.password || !formData.passwordCheck) {
-            setShake(true);
-            setTimeout(() => setShake(false), 500);
-            console.log("Some fields are not filled-in!");
-            setTimeout(() => {
-                alert("Some fields are not filled-in!");
-            }, 400);
-
-            return;
-        }
-
-        if (formData.password != formData.passwordCheck) {
-            setShake(true);
-            setTimeout(() => setShake(false), 500);
-            console.log("The passwords are not the same!");
-            setTimeout(() => {
-                alert("The passwords are not the same!");
-            }, 400);
-
-            return;
-        }
-
-        console.log(formData);
-
-    };
+    const errMessage = "Field is required";
 
     return (
         <>
             <div className={styles.container}>
-                <form className={`${styles.container__form} ${shake ? styles["shake"] : ""}`} onSubmit={handleSubmit}>
+                <form className={`${styles["container__form"]} ${shake ? styles["shake"] : ""}`} onSubmit={handleSubmit(onSubmit, onInvalid)}>
                     <div className={styles["container__form-headline"]}>Sign Up</div>
-                    <input className={styles["container__form-input"]} type="text" name="login" placeholder="Login" value={formData.login} onChange={handleChange} />
+                    <input {...register("login", { required: errMessage })}
+                        className={`${styles["container__form-input"]} ${errors.login ? styles.error : ""}`} type="text" placeholder="Login" />
+                    <p className={styles["container__form-error"]}>{errors.login?.message}</p>
 
-                    <input className={styles["container__form-input"]} type="email" name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
+                    <input {...register("email", { required: errMessage, pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Not an email" } })} type="text"
+                        className={`${styles["container__form-input"]} ${errors.email ? styles.error : ""}`} placeholder="E-mail" />
+                    <p className={styles["container__form-error"]}>{errors.email?.message}</p>
 
-                    <input className={styles["container__form-input"]} type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} />
+                    <input {...register("password", { required: errMessage })}
+                        className={`${styles["container__form-input"]} ${errors.password ? styles.error : ""}`} type="password" placeholder="Password" />
+                    <p className={styles["container__form-error"]}>{errors.password?.message}</p>
 
-                    <input className={styles["container__form-input"]} type="password" name="passwordCheck" placeholder="Password check" value={formData.passwordCheck} onChange={handleChange} />
+                    <input {...register("passwordCheck", { required: errMessage, validate: value => value === password.current || "The passwords do not match" })}
+                        className={`${styles["container__form-input"]} ${errors.passwordCheck ? styles.error : ""}`} type="password" placeholder="Password check" />
+                    <p className={styles["container__form-error"]}>{errors.passwordCheck?.message}</p>
+
                     <button className={styles["container__form-button"]} type="submit">
                         Send
                     </button>
