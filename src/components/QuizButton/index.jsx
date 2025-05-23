6e470f71d1b62cd05c7c7a33be263b80d5bcbd23
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-05-23 21:21:38 +0200

    Show real questions from db

--- src/components/QuizButton/index.jsx
+++ src/components/QuizButton/index.jsx
@@ -6,9 +6,9 @@ import styles from "./QuizButton.module.scss";
 export default function QuizButton({ name, status, score, id }) {
 
   const navigate = useNavigate();
-  
-return (
-    <button className={styles.container} onClick={() => navigate(`/quiz/${id}`)} key={name} value={name}>
+
+  return (
+    <button className={styles.container} onClick={() => navigate(`/quizzes/${id}`)} key={name} value={name}>
       <div>{name}:  <span style={{ color: "#9F50B1" }}>{status}</span></div>
       <div>{score}</div>
     </button>
