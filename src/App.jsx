c875d2f219fa9649cfae239e53e88b4a6f045071
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-05-29 18:04:08 +0200

    Copy registration style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Navigate, Routes } from "react-router-dom";
 
+import EnterForm from "./components/EnterForm";
 import ListQuizzes from "./components/ListQuizzes";
 import QuizCard from "./components/QuizCard";
 import RegisterForm from "./components/RegisterForm";
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/quiz" element={<ListQuizzes />} />
         <Route path="/signup" element={< RegisterForm />} />
+        <Route path="/signin" element={<EnterForm />} />
         <Route path="/quiz/:id" element={<QuizCard />} />
         <Route path="/" element={<Navigate to="/quiz" replace />} />
       </Routes>
---
+++ src/components/EnterForm/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState, useRef } from "react";
+import { useForm } from "react-hook-form";
+
+import regiFormImg from "../../assets/Frame5_2.svg";
+import styles from "../RegisterForm/RegisterForm.module.scss";
+
+export default function EnterForm() {
+    const { register, handleSubmit, formState: { errors }, watch } = useForm({ defaultValues: { login: "", email: "", password: "", passwordCheck: "" } });
+    const [shake, setShake] = useState(false);
+    const password = useRef({});
+
+    password.current = watch("password");
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+    const onInvalid = () => {
+        setShake(true);
+        setTimeout(() => setShake(false), 500);
+    };
+
+    const errMessage = "This field is required";
+
+    return (
+        <>
+            <div className={`${styles.container} ${styles["container__signInVariation"]}`}>
+                <form className={`${styles["container__form"]} ${shake ? styles["shake"] : ""}`} onSubmit={handleSubmit(onSubmit, onInvalid)}>
+                    <div className={styles["container__form-headline"]}>Sign In</div>
+
+                    <input {...register("email", { required: errMessage, pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Invalid email" } })} type="text"
+                        className={`${styles["container__form-input"]} ${errors.email ? styles.error : ""}`} placeholder="E-mail" />
+                    {errors.email && <p className={styles["container__form-error"]}>{errors.email.message}</p>}
+
+                    <input {...register("password", { required: errMessage, minLength: { value: 6, message: "Password must have at least 6 characters" } })}
+                        className={`${styles["container__form-input"]} ${errors.password ? styles.error : ""}`} type="password" placeholder="Password" />
+                    {errors.password && <p className={styles["container__form-error"]}>{errors.password.message}</p>}
+
+                    <button className={styles["container__form-button"]} type="submit">
+                        OK
+                    </button>
+                </form>
+                <img className={styles.container__img} src={regiFormImg} alt="Registration illustration" />
+            </div>
+        </>
+    )
+}
