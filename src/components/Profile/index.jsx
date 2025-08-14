800b789f00b5da6e132cf75f641da084c1042f7b
maya.molodec <21056935+mayamolodec@users.noreply.github.com>
2025-08-14 19:09:16 +0200

    Add simple profile page

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+
+import { useGetCurrentUserQuery } from "../../store/quizApi";
+
+import styles from "./Profile.module.scss";
 
 export default function Profile() {
-    return <h1> Profile</h1>
+    const { data, error, isLoading } = useGetCurrentUserQuery();
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <Navigate to="/sign-in" />
+    }
+
+    return (
+        <div className={styles["container"]}>
+            <div className={styles["container__card"]}>
+                <p> <span style={{ color: "#9F50B1" }}>Name:</span> {data.name}</p>
+                <p> <span style={{ color: "#9F50B1" }}>Email:</span> {data.email}</p>
+            </div>
+
+        </div>
+    )
+
 }
--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -4,9 +4,8 @@ import { Navigate } from "react-router-dom";
 import { useGetCurrentUserQuery } from "../store/quizApi";
 
 const PrivateRoute = ({ children }) => {
-    const { data, error, isLoading } = useGetCurrentUserQuery();
+    const { error, isLoading } = useGetCurrentUserQuery();
 
-    console.log(data);
     if (isLoading) return <div>Loading...</div>;
 
     if (error) return <Navigate to="/sign-in" />;
